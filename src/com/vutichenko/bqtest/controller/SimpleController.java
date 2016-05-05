1b1464a6c89af3ad42f3c22a3cf5f066f75d93da
holydos <holydos@rambler.ru>
2016-05-05 16:51:25 +0300

    added basic blocking queue logic, with invalid architecture

--- src/com/vutichenko/bqtest/controller/SimpleController.java
+++ src/com/vutichenko/bqtest/controller/SimpleController.java
@@ -9,20 +9,73 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 /**
  * Created by HolyDos on 28.04.2016.
  */
 @org.springframework.stereotype.Controller
-public class SimpleController  {
+public class SimpleController {
+
+    EntityManagerUtil managerUtil = new EntityManagerUtil();
+    int limit = 10;
 
     @RequestMapping(value = "/index.htm", method = RequestMethod.GET)
     public ModelAndView showIndex() {
         ModelAndView mv = new ModelAndView("/WEB-INF/jsp/display.jsp");
-        EntityManagerUtil.savePojoEntity("a random 2");
+        SimpleEntityProducer producer = new SimpleEntityProducer();
+        SimpleEntityConsumer consumer = new SimpleEntityConsumer();
+        producer.start();
+        try {
+            Thread.currentThread().sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        consumer.start();
+       /* try {
+            managerUtil.savePojoEntity("a random 2",10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         SimpleEntity se = EntityManagerUtil.getEntityList().iterator().next();
-        mv.getModelMap().addAttribute("id",se.getEntityId());
-        mv.getModelMap().addAttribute("randomValue",se.getRandomValue());
+        mv.getModelMap().addAttribute("id", se.getEntityId());
+        mv.getModelMap().addAttribute("randomValue", se.getRandomValue());*/
         return mv;
+    }
+
+    private class SimpleEntityProducer extends Thread {
+
+
+        @Override
+        public void run() {
+
+
+
+            while (true) {
+                try {
+                    managerUtil.savePojoEntity("a random 2", limit);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    private class SimpleEntityConsumer extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                List<SimpleEntity> entities = EntityManagerUtil.getEntityList();
+                try {
+                    if(entities.size()>0)
+                    managerUtil.removeSimpleEntity(entities.get(entities.size()-1).getEntityId(),limit);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @RequestMapping(value = "/employee", method = RequestMethod.GET)
@@ -31,7 +84,7 @@ public class SimpleController  {
     }
 
     @RequestMapping(value = "/display.htm", method = RequestMethod.POST)
-    public String submit(@ModelAttribute("pojoEntity")SimpleEntity simpleEntity,
+    public String submit(@ModelAttribute("pojoEntity") SimpleEntity simpleEntity,
                          BindingResult result, ModelMap model) {
         if (result.hasErrors()) {
             return "error";
--- src/com/vutichenko/bqtest/service/EntityManagerUtil.java
+++ src/com/vutichenko/bqtest/service/EntityManagerUtil.java
@@ -1,11 +1,12 @@
 package com.vutichenko.bqtest.service;
 
+import com.sun.istack.internal.NotNull;
 import com.vutichenko.bqtest.pojo.SimpleEntity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,6 +14,7 @@ import java.util.List;
  */
 public class EntityManagerUtil {
     private static final EntityManagerFactory entityManagerFactory;
+
     static {
         try {
             entityManagerFactory = Persistence.createEntityManagerFactory("persistenceUnit");
@@ -23,35 +25,82 @@ public class EntityManagerUtil {
         }
     }
 
+    public static int entityQuantity() {
+        return getEntityList().size();
+    }
+
     public static EntityManager getEntityManager() {
         return entityManagerFactory.createEntityManager();
     }
 
-    public static SimpleEntity savePojoEntity(String randomValue) {
+    public synchronized SimpleEntity savePojoEntity(String randomValue, int limit) throws InterruptedException {
         EntityManager manager = getEntityManager();
+
+        System.out.println("start save");
+        Thread.sleep(2000);
+        if (getEntityList().size() == limit) {
+            System.out.println("save notify");
+            notifyAll();
+        }
+        while (getEntityList().size() == limit) {
+            System.out.println("limit size");
+
+            wait();
+        }
         SimpleEntity student = new SimpleEntity();
         try {
             manager.getTransaction().begin();
             student.setRandomValue(randomValue);
             student = manager.merge(student);
             manager.getTransaction().commit();
+            System.out.println("saved id " + student.getEntityId());
         } catch (Exception e) {
             manager.getTransaction().rollback();
         }
         return student;
     }
 
+    public synchronized void removeSimpleEntity(Long entityId, int limit) throws InterruptedException {
+        EntityManager entityManager = getEntityManager();
+
+        System.out.println("start remove");
+        Thread.sleep(2000);
+
+        if (getEntityList().size() < limit) {
+            System.out.println("remove notify");
+            notifyAll();
+        }
+
+        while (getEntityList().size() == 0) {
+            System.out.println("zero size");
+            wait();
+        }
+
+        try {
+            entityManager.getTransaction().begin();
+            SimpleEntity simpleEntity = (SimpleEntity) entityManager.find(SimpleEntity.class, entityId);
+            entityManager.remove(simpleEntity);
+            entityManager.getTransaction().commit();
+            System.out.println("removed" + simpleEntity.getEntityId());
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+        }
+
+    }
+
+    @NotNull
     public static List<SimpleEntity> getEntityList() {
         EntityManager manager = getEntityManager();
         try {
             manager.getTransaction().begin();
             List<SimpleEntity> list = (List<SimpleEntity>) manager.createQuery("FROM SimpleEntity ").getResultList();
+            if (list == null) throw new IllegalStateException("something went wrong");
             manager.getTransaction().commit();
             return list;
         } catch (Exception e) {
             manager.getTransaction().rollback();
         }
-        return null;
+        return Collections.emptyList();
     }
 }
 
